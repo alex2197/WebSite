0f0ddf4c11de64d33597c30d8ee4b94386203f90
Alejandro <Alex21_97@hotmail.com>
2021-01-17 23:34:23 -0700

    commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var mysql = require('mysql');
-var http = require('http')
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -9,18 +8,6 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err){
-    if(err){
-        throw err
-    } 
-    console.log("Connected!")
-    var fullName = req.first_name;
-    var lastName = req.last_name;
-    var email = req.email;
-    var sql = `INSERT INTO contact(first_name, last_name, email) VALUES (${fullName}, ${lastName}, ${email});`
-    con.query(sql, function (err){
-        if(err){
-            throw err
-        }
-        console.log(fields);
-    })
+    if(err) throw err;
+    console.log("Connected!");
 });
